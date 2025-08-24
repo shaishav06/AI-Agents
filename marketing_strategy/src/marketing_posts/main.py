961d9c93f3ab372f3870e8154eb1e31dc41641c2
shaishav <shaishavsurati06@gmail.com>
2025-08-24 16:07:52 +0530

    Marketing Strategy added

--- marketing_strategy/src/marketing_posts/main.py
+++ marketing_strategy/src/marketing_posts/main.py
@@ -6,13 +6,13 @@ from marketing_posts.crew import MarketingPostsCrew
 def run():
     # Replace with your inputs, it will automatically interpolate any tasks and agents information
     inputs = {
-        'customer_domain': 'crewai.com',
-        'project_description': """
-CrewAI, a leading provider of multi-agent systems, aims to revolutionize marketing automation for its enterprise clients. This project involves developing an innovative marketing strategy to showcase CrewAI's advanced AI-driven solutions, emphasizing ease of use, scalability, and integration capabilities. The campaign will target tech-savvy decision-makers in medium to large enterprises, highlighting success stories and the transformative potential of CrewAI's platform.
+        "customer_domain": "nvidia.com/en-in/ai/",
+        "project_description": """
+nvidia, a leading provider of NIMs, aims to revolutionize marketing automation for its enterprise clients. This project involves developing an innovative marketing strategy to showcase nvidia's NIMs, emphasizing ease of use, scalability, and integration capabilities. The campaign will target tech-savvy decision-makers in medium to large enterprises, highlighting success stories and the transformative potential of nvidia's platform.
 
 Customer Domain: AI and Automation Solutions
-Project Overview: Creating a comprehensive marketing campaign to boost awareness and adoption of CrewAI's services among enterprise clients.
-"""
+Project Overview: Creating a comprehensive marketing campaign to boost awareness and adoption of nvidia's services among enterprise clients.
+""",
     }
     MarketingPostsCrew().crew().kickoff(inputs=inputs)
 
@@ -22,13 +22,13 @@ def train():
     Train the crew for a given number of iterations.
     """
     inputs = {
-        'customer_domain': 'crewai.com',
-        'project_description': """
-CrewAI, a leading provider of multi-agent systems, aims to revolutionize marketing automation for its enterprise clients. This project involves developing an innovative marketing strategy to showcase CrewAI's advanced AI-driven solutions, emphasizing ease of use, scalability, and integration capabilities. The campaign will target tech-savvy decision-makers in medium to large enterprises, highlighting success stories and the transformative potential of CrewAI's platform.
+        "customer_domain": "nvidia.com",
+        "project_description": """
+nvidia, a leading provider of gpus, aims to revolutionize marketing automation for its enterprise clients. This project involves developing an innovative marketing strategy to showcase nvidia's advanced gpu, emphasizing ease of use, scalability, and integration capabilities. The campaign will target tech-savvy decision-makers in medium to large enterprises, highlighting success stories and the transformative potential of nvidia's platform.
 
 Customer Domain: AI and Automation Solutions
-Project Overview: Creating a comprehensive marketing campaign to boost awareness and adoption of CrewAI's services among enterprise clients.
-"""
+Project Overview: Creating a comprehensive marketing campaign to boost awareness and adoption of nvidia's services among enterprise clients.
+""",
     }
     try:
         MarketingPostsCrew().crew().train(n_iterations=int(sys.argv[1]), inputs=inputs)
